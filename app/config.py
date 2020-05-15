38f148ad681fd7b89a6731543ed70d28ba7ce0e0
gabastil <glenn.abastillas@gmail.com>
2020-05-15 16:16:48 -0400

    Updated config and added venv

--- app/config.py
+++ app/config.py
@@ -1,6 +1,5 @@
 import os
 
-
 class Config():
     SECRET_KEY = os.environ.get("SECRET_KEY") or ""
     SQLALCHEMY_DATABASE_URI = "mysql://gabastil1@{SECRET_KEY}gabastil1.db.8756087.hostedresource.com/gabastil1"
